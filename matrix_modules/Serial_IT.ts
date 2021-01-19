a0d5589ba2c254fbff69b468182714a17a3d279d
DCisHurt <roger33669@gmail.com>
2021-01-19 17:50:46 +0800

    update serial

---
+++ matrix_modules/Serial_IT.ts
@@ -0,0 +1,30 @@
+namespace Serial_IT{
+
+    export function pInt(str: string): number {
+        for (let i = 0; i < 2; i ++) {
+            if (!(parseInt(str.charAt(i), 16))){
+                return -1
+            }
+        }
+        return parseInt(str, 16)
+    }
+
+    export function setMicro(func: number, para: number): void {
+        switch (func)
+        {
+            case 1:
+            if(para > 127){
+                Matrix.motor(M_PORT.M1, para-256);
+            }
+            else{
+                Matrix.motor(M_PORT.M1, para);
+            }
+            break;
+        }
+  
+    }
+
+    // export function getMicro(func: number): string {
+
+    // }
+}
